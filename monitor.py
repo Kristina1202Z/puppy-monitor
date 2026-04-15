b774cf24451ef2b7c0251cf073a036103df8b837
KristinaZ <94491788+Kristina1202Z@users.noreply.github.com>
2026-04-14 21:19:27 -0400

    Implement email notifications for website updates
    Added email notification functionality to alert users about updates.

--- monitor.py
+++ monitor.py
@@ -3,10 +3,26 @@ from bs4 import BeautifulSoup
 import hashlib
 import os
 
+import smtplib
+from email.mime.text import MIMEText
+
 URL = "https://www.stoneyacrepuppies.ca/ready-for-reservation.html"
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
+
+def send_email(msg):
+    user = os.environ["EMAIL_USER"]
+    password = os.environ["EMAIL_PASS"]
+
+    msg_obj = MIMEText(msg)
+    msg_obj["Subject"] = "🐶 Puppy Update!"
+    msg_obj["From"] = user
+    msg_obj["To"] = user
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(user, password)
+        server.send_message(msg_obj)
 
 def send_msg(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -30,8 +46,9 @@ def main():
     else:
         old_hash = ""
 
-    if old_hash and new_hash != old_hash:
-        send_msg("🐶 网站更新了")
+if old_hash and new_hash != old_hash:
+    send_msg("🐶 网站更新")
+    send_email("🐶 网站更新")
 
     with open("last.txt", "w") as f:
         f.write(new_hash)
